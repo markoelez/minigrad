540e3670852e9777e91c3b5103058c06c1b54a3c
Marko Elez <markoelez7@gmail.com>
2022-02-21 22:56:40 -0800

    use random test fixtures

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,46 +1,47 @@
+import random
+import math
 import torch
 import numpy as np
 from minigrad.tensor import Tensor
 from minigrad.function import Mul, Add
 
-def test_grads():
-    # minigrad
-    a = Tensor(2)
-    b = Tensor(3)
-    c = Tensor(4)
-    d = Tensor(5)
 
+def get_data():
+    x = random.uniform(0, 1000)
+    return Tensor(x), torch.tensor(x, requires_grad=True)
+
+def is_close(a, b):
+    return math.isclose(a, b, rel_tol=1e-07)
+
+def test_scalar_grads():
+    # fixtures
+    a, aa = get_data()
+    b, bb = get_data()
+    c, cc = get_data()
+    d, dd = get_data()
+    
+    # minigrad ops
     x = Mul.apply(a, b)
     y = Mul.apply(c, d)
-
     z = Add.apply(x, y)
-
     z.backward()
     
-    # pytorch
-    aa = torch.tensor(2., requires_grad=True)
-    bb = torch.tensor(3., requires_grad=True)
-    cc = torch.tensor(4., requires_grad=True)
-    dd = torch.tensor(5., requires_grad=True)
-    
+    # pytorch ops
     xx = aa * bb
     yy = cc * dd
-
     zz = xx + yy
-
     zz.backward()
 
-    # assert
-    assert aa.detach().numpy() == a.data
-    assert bb.detach().numpy() == b.data
-    assert cc.detach().numpy() == c.data
-    assert dd.detach().numpy() == d.data
+    # ensure values are equal
+    assert is_close(aa.detach().item(), a.data)
+    assert is_close(bb.detach().item(), b.data)
+    assert is_close(cc.detach().item(), c.data)
+    assert is_close(dd.detach().item(), d.data)
+    assert is_close(xx.detach().item(), x.data)
+    assert is_close(yy.detach().item(), y.data)
+    assert is_close(zz.detach().item(), z.data)
 
-    assert xx.detach().numpy() == x.data
-    assert yy.detach().numpy() == y.data
-
-    assert zz.detach().numpy() == z.data
-
+    # ensure gradients are equal
     assert aa.grad == a.grad
     assert bb.grad == b.grad
     assert cc.grad == c.grad
